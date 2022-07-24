7abbecbd92014c8b22988e42a66ecd8f7604eaa6
Kavisha Chiranjaya Abeynayaka <87388691+kaviabey@users.noreply.github.com>
2022-07-24 17:49:43 +0530

    JS file for the 1.html page

---
+++ Assignment.js
@@ -0,0 +1,7 @@
+
+let input =  document.getElementsByTagName("input");
+
+console.log(btn);
+function testrun(){
+  window.alert("Your name is " +input[0].value+" and you are from "+input[1].value);
+}
